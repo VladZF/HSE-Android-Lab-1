9ab1f6275238e17540beb66c39218dac26bebd97
VladZF <vlad2003587@gmail.com>
2024-10-25 23:59:30 +0300

    First working version

--- app/src/main/java/com/example/firstapphse/MainActivity.java
+++ app/src/main/java/com/example/firstapphse/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.firstapphse;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView greetingBox;
+    private TextView greetingNameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        greetingBox = findViewById(R.id.greetingBox);
+        greetingNameText = findViewById(R.id.greetingNameText);
+    }
 
+    public void goToNameActivity(View view) {
+        Intent intent = new Intent(this, NameActivity.class);
+        intent.putExtra("greeting", greetingBox.getText().toString());
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) return;
+        String fullGreeting = data.getStringExtra("full_greeting");
+        greetingNameText.setText(fullGreeting);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/example/firstapphse/NameActivity.java
+++ app/src/main/java/com/example/firstapphse/NameActivity.java
@@ -1,19 +1,38 @@
 package com.example.firstapphse;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class NameActivity extends AppCompatActivity {
 
+    private TextView nameBox;
+    private String greetingString;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_name);
+        Intent intent = getIntent();
+        nameBox = findViewById(R.id.nameBox);
+        TextView greeting = findViewById(R.id.savedGreetingText);
+        greetingString = intent.getStringExtra("greeting");
+        greetingString = greetingString.isEmpty() ? "Hello" : greetingString;
+        greeting.setText("Greeting: " + greetingString);
+    }
+
+    public void GetBackToMainActivity(View view) {
+        Intent intent = new Intent();
+        String name = nameBox.getText().toString();
+        intent.putExtra("full_greeting", greetingString + " " + name);
+        setResult(1, intent);
+        finish();
     }
 }
